bf297016f4713c106ea3d0bef66eef16449fdf11
Sandra Cardona <sandracardona5@gmail.com>
2018-10-08 04:57:05 +1100

    Update Readme.cmd

--- Readme.cmd
+++ Readme.cmd
@@ -1,4 +1,4 @@
-DiUS is starting a computer store. You have been engaged to build the checkout system. We will start with the following products in our catalogue
+You have been engaged to build the checkout system. We will start with the following products in our catalogue
 
 
 | SKU     | Name        | Price    |
